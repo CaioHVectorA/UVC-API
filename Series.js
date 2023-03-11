62887e7ad9a16665653ff0908185dec079410ecb
Caio Henrique Oliveira Batista <caihebatista@gmail.com>
2023-03-11 13:57:37 -0300

    adicionado 1 solo e 1 series

--- Series.js
+++ Series.js
@@ -2,8 +2,8 @@ let Series = []
 
 function CSe(Nome,Ref,Desc,Local,Lancamento,Status,Escrito,Tags,Imgref,Associados,Capitulos) {
 this.Nome = Nome,
-this.Tipo = "SOLO",
-this.Ref = Ref,
+this.Tipo = "SERIES",
+this.Ref = `SE_${Ref}`,
 this.Desc = Desc,
 this.Local = Local,
 this.Lancamento = Lancamento,
@@ -14,4 +14,99 @@ this.Imgref = Imgref,
 this.Associados = Associados
 this.Capitulos = Capitulos
 }
+
+const MR1 = new CSe('Menino Resilient I','MR1','Acompanhe a história do guardião substituto,que ganhou seus poderes em meio a desastres que mudaram toda sua vida.','TERRA','11/03/2023','Completo','Caio Henrique',['Drama','Terra','Ação','Teen'],'https://live.staticflickr.com/65535/52739881142_cc750f9fba_m.jpg',['SE_GUA','S_KER'],[{
+    Nome: "Uma Nova Luz",
+    Disponivel: true,
+    NumEps: 3,
+    DescCap: "Lorem Ipsum blablabla",
+    Episodios: {
+        Ep1: {
+            Nome: 'Desejos',
+            Ref: '01.txt',
+            Numero: 1
+        },
+        Ep2: {
+            Nome: 'O Experimento',
+            Ref: '02.txt',
+            Numero: 2
+        },
+        Ep3: {
+            Nome: 'Sonhos',
+            Ref: '03.txt',
+            Numero: 3
+        }
+    }
+},
+{
+    Nome: "Resiliência e Persistência",
+    Disponivel: true,
+    NumEps: 3,
+    DescCap: "Lorem Ipsum blablabla",
+    Episodios: {
+        Ep4: {
+            Nome: 'Contra todos, contra tudo',
+            Ref: '04.txt',
+            Numero: 4
+        },
+        Ep5: {
+            Nome: 'Superações',
+            Ref: '05.txt',
+            Numero: 5
+        },
+        Ep6: {
+            Nome: 'Novos Desafios',
+            Ref: '06.txt',
+            Numero: 6
+        }
+    }
+},
+{
+    Nome: "Persistência e Ascensão",
+    Disponivel: true,
+    NumEps: 3,
+    DescCap: "blablabla",
+    Episodios: {
+        Ep7: {
+            Nome: 'A sombra da noite',
+            Ref: '07.txt',
+            Numero: 7
+        },
+        Ep8: {
+            Nome: 'Predador e a presa',
+            Ref: '08.txt',
+            Numero: 8
+        },
+        Ep9: {
+            Nome: 'Repetições sem fim',
+            Ref: '09.txt',
+            Numero: 9
+        }
+    }
+},
+{
+    Nome: "Ascensão e Vitória!",
+    Disponivel: true,
+    NumEps: 3,
+    DescCap: "blablabla",
+    Episodios: {
+        Ep10: {
+            Nome: 'Sem descanso',
+            Ref: '10.txt',
+            Numero: 10
+        },
+        Ep11: {
+            Nome: 'Dois por um',
+            Ref: '11.txt',
+            Numero: 11
+        },
+        Ep12: {
+            Nome: 'Vitória ou Auto-Ilusão?',
+            Ref: '12.txt',
+            Numero: 12
+        }
+    }
+}
+])
+Series.push(MR1)
 module.exports = Series
--- Solos.js
+++ Solos.js
@@ -13,7 +13,8 @@ this.Tags = Tags,
 this.Imgref = Imgref,
 this.Associados = Associados
 }
-const Hades = new CS('Hades','HAD','Conheça profundamente a origem e os objetivos do Monstruoso e Soberano Demônio, que pretende dominar todo o universo com seu poder divino e maligno.','UNIVERSAL','j','Completo','Caio Henrique',['Drama','Universal','Ação','Violência'],'',["SE_CIEC","SE_CONG","SE_GUA"])
-console.log('Hades:',Hades)
-Solos.push(Hades)
+const Hades = new CS('Hades','HAD','Conheça profundamente a origem e os objetivos do Monstruoso e Soberano Demônio, que pretende dominar todo o universo com seu poder divino e maligno.','UNIVERSAL','10/03/2003','Completo','Caio Henrique',['Drama','Universal','Ação','Violência'],'https://live.staticflickr.com/65535/52739035768_37250eb886_n.jpg',["SE_CIEC","SE_CONG","SE_GUA"])
+const Ciecon = new CS('Ciecon','CIEC','Leia a história origem difícil do justiceiro espacial, Ciecon. Que posteriormente avançou a jornada e se tornou o líder dos Congregadores.','UNIVERSAL','10/03/2003','Completo','Caio Henrique',['Drama','Universal','Sci-Fi'],'https://live.staticflickr.com/65535/52738589846_0158b4f28c_w.jpg',['SE_CONG','S_HAD','SE_GUA'])
+
+Solos.push(Hades,Ciecon)
 module.exports = Solos

006ca17c89bd56779b54c6b360061334a98c84bf
Caio Henrique Oliveira Batista <caihebatista@gmail.com>
2023-03-20 22:59:55 -0300

    mais rotas

--- Routes.js
+++ Routes.js
@@ -38,13 +38,55 @@ Routes.get('/Solos',async (req, res) => {
     
     return res.json(db)
 })
+Routes.get('/Solos/:id',async (req, res) => {
+    const { id } = req.params
+    const db = []
+    db.push(Solos)
+    console.log(db[id * 1],id * 1,db[0])
+    return res.json(db[0][id * 1])
+})
+Routes.get('/Solos/Alternatives',async (req, res) => {
+    let Alt = Solos.filter(item => item.isAlternative === true)
+    console.log(Solos)
+    const db = []
+    Alt.forEach(element => {
+      db.push(element) 
+    });
+    // db.push(Solos)
+    
+    return res.json(db)
+})
 Routes.get('/Series',async (req, res) => {
     const db = []
     db.push(Series)
     
     return res.json(db)
 })
+Routes.get('/Series/:REF',async (req, res) => {
+    const {REF} = req.params 
+    let db = []
+    const refe = Series.filter(item => item.Ref === `SE_${REF}`)
+    console.log(refe,db,Series)
+    db = refe
+    return res.json(db)
+})
 
+Routes.get('/Series/:REF/Cap',async (req, res) => {
+    const {REF} = req.params 
+    let db = []
+    const refe = Series.filter(item => item.Ref === `SE_${REF}`)
+    console.log(refe,db,Series)
+    db = refe
+    return res.json(db[0].Capitulos)
+})
+// Routes.get('/Series/:REF/EP/:NUM',async (req, res) => {
+//     const {NUM} = req.params 
+//     let db = []
+//     const refe = Series.filter(item => item.Ref === `SE_${REF}`)
+//     console.log(refe,db,Series)
+//     db = refe
+//     return res.json(db[0].Capitulos)
+// })
 Routes.get('/', async (req, res) => {
     const db = []
     db.push(Solos,Series)
--- Solos.js
+++ Solos.js
@@ -1,5 +1,4 @@
 let Solos = []
-
 function CS(Nome,Ref,Desc,Local,Lancamento,Status,Escrito,Tags,Imgref,Associados,Alt) {
 this.Nome = Nome,
 this.Tipo = "SOLO",
@@ -18,5 +17,5 @@ const Hades = new CS('Hades','HAD','Conheça profundamente a origem e os objetiv
 const Ciecon = new CS('Ciecon','CIEC','Leia a história origem difícil do justiceiro espacial, Ciecon. Que posteriormente avançou a jornada e se tornou o líder dos Congregadores.','UNIVERSAL','10/03/2003','Completo','Caio Henrique',['Drama','Universal','Sci-Fi'],'https://live.staticflickr.com/65535/52738589846_0158b4f28c_w.jpg',['SE_CONG','S_HAD','SE_GUA'],false)
 const Kerry = new CS('Kerry','KER','O Sistema destruiu a vida de Kerry. Mas quando ele se levanta,seu único desejo é a vingança.','TERRA','11/03/2023','Completo','Caio Henrique',['Violência','Ação','Drama'],'https://live.staticflickr.com/65535/52739030508_e60672c0fd_w.jpg',['SE_GUA','SE_MR2'],false)
 const OGFDM = new CS('O Glacial Fim do mundo','GFM','Cole Froz chega no planeta terra,e em poucas horas já causa problemas severos o suficiente para causar um apocalipse em todo o globo.','TERRA','14/03','COMPLETO','Caio Henrique',['Ação','Sci-Fi','Alternativo'],'https://live.staticflickr.com/65535/52748630855_758ce69294_n.jpg',['SE_GUA'],true)
-Solos.push(Hades,Ciecon,Kerry)
+Solos.push(Hades,Ciecon,Kerry,OGFDM)
 module.exports = Solos
